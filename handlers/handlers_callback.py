500a3aa02991acb1a125c26f2bac4ecee3e9f4e0
reelax_bot.py <rylaksmanyjrrelaxmgr@gmail.com>
2026-09-09 15:54:36 +0300

    Update handlers_callback.py

--- handlers/handlers_callback.py
+++ handlers/handlers_callback.py
@@ -44,7 +44,7 @@ from utils import (
     get_text, StateManager, UserState,
     KeyboardFactory, CB, get_ram_usage, RATE_LIMITER
 )
-from .handlers_command import CommandHandlers, CACHE
+from .handlers_command import CommandHandlers
 from cache import (
     user_cache, invalidate_user_cache,
     banned_words_cache, settings_cache,
